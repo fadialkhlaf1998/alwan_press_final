c83dae714b2193344d80a26824fcaa31b0c0108a
imac <fadialkhlaf1998>
2022-08-15 11:28:19 +0400

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/alwan_press/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.alwan_press
+import android.app.NotificationManager
+import android.content.Context
+import io.flutter.embedding.android.FlutterActivity
+
+
+class MainActivity: FlutterActivity() {
+
+    override fun onResume() {
+        super.onResume()
+        closeAllNotifications();
+    }
+
+    private fun closeAllNotifications() {
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.cancelAll()
+    }
+
+}
